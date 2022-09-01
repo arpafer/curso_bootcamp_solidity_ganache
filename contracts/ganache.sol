c0b39ddae85eef141fdb1e6bc1a7bc70ef79e55a
Remix Workspace <>
2022-09-01 19:12:35 +0100

    Primer commit

---
+++ contracts/ganache.sol
@@ -0,0 +1,28 @@
+/// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract ganache {
+
+    address public owner;
+    address public smartContract;
+    string message = "";
+
+    event newContract(address owner, address smartContract);
+    event newMessage(string mensaje);
+
+    constructor() {
+        owner = msg.sender;
+        smartContract = address(this);
+        emit newContract(owner, smartContract);
+    }
+
+    function setMessage(string memory _message) public {
+        message = _message;
+        emit newMessage(_message);
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
